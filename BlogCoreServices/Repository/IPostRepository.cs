0f3a504c8626c2f37509cae9e0bc973d645fa389
Nadeem4913 <nadeem.it.786@hotmail.com>
2021-05-01 12:51:12 +0530

    Intial Commit

---
+++ BlogCoreServices/Repository/IPostRepository.cs
@@ -0,0 +1,23 @@
+﻿using BlogCoreServices.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogCoreServices.Repository
+{
+    public interface IPostRepository
+    {
+        Task<List<Category>> GetCategories();
+
+        Task<List<PostViewModel>> GetPosts();
+
+        Task<PostViewModel> GetPost(int? postId);
+
+        Task<int> AddPost(Post post);
+
+        Task<int> DeletePost(int? postId);
+
+        Task UpdatePost(Post post);
+    }
+}
